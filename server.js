313c1333b7d1e9490a2f48efdde1d21639badd28
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-01 22:25:59 +0900

    ✨ダミーページ追加

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
- const app = express(); 
- const port = 3000; 
+const apiRouter = require("./routes/api");
 
- app.get("/", (req, res, next) => { 
-   res.end("Top Page");
- });
+const app = express(); 
+const port = 3000; 
+
+app.use(express.static('public'));
+
+// apiのルーティング
+app.use("/api", apiRouter);
 
  // サーバを起動する
  app.listen(port, () => { 
    console.log(`Server is running on http://localhost:${port}`);
  });
-
