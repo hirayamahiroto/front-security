717c5b6977483d8972f2d1c26388b6d6aee96c7d
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-04 20:39:34 +0900

    ♻︎ リファクタ

--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,26 @@ const router = express.Router();
 
 router.use(express.json());
 router.get("/get", (req, res, next) => {
-    logger.debug("GET /get endpoint called");
+  logger.debug("GET /get endpoint called");
 
-    res.setHeader("X-Timestamp", Date.now());
+  res.setHeader("X-Timestamp", Date.now());
 
-    let message = req.query.message;
-    const lang = req.headers["x-lang"];
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
 
-    if (!message) {
-        res.status(400);
-        message = lang === "ja" ? "リクエストが不正です" : "Bad Request";
-    }
+  if (!message) {
+    res.status(400);
+    message = lang === "ja" ? "リクエストが不正です" : "Bad Request";
+  }
 
-
-    res.send({message});
+  res.send({ message });
 });
 
 router.use(express.json());
 router.post("/post", (req, res, next) => {
-    const body = req.body;
-    console.log(body);
-    res.json({ status: 'success', data: body });
+  const body = req.body;
+  console.log(body);
+  res.json({ status: "success", data: body });
 });
 
 module.exports = router;
