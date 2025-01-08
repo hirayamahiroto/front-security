4690d0f4a2a94d6de3b47ecbfa9c9888e46b3f08
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-08 22:51:17 +0900

    ✨ csp追加：jsを無効化

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-
 app.use("/api", cors, apiRouter);
+
+// ejsの設定
+app.set("view engine", "ejs");
+app.get("/csp", (req, res, next) => {
+  res.header("Content-Security-Policy", "script-src 'self'");
+  res.render("csp");
+});
 
 // サーバを起動する
 app.listen(port, () => {
