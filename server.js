7503c38aeef2142294f8edb32f8ef551bd6a514d
HIROTO <123345463+hirayamahiroto@users.noreply.github.com>
2025-01-11 18:32:53 +0900

    ♻️ hansonに必要のないルーティングはコメントアウト

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const apiRouter = require("./routes/api");
+const csrfRouter = require("./routes/csrf");
 const { cors } = require("./middleware");
 
 const app = express();
 const port = 3000;
 
-// CSPの設定
-app.use((req, res, next) => {
-  const nonce = crypto.randomBytes(16).toString("base64");
+// // CSPの設定
+// app.use((req, res, next) => {
+//   const nonce = crypto.randomBytes(16).toString("base64");
 
-  // CSP（同一オリジンのリソースのみ読み込み可能、nonceを使用してスクリプトを読み込み可能にする（外部から挿入されたscriptは実行さない））
-  res.setHeader(
-    "Content-Security-Policy",
-    `default-src 'self'; script-src 'self' 'nonce-${nonce}'`
-  );
+//   // CSP（同一オリジンのリソースのみ読み込み可能、nonceを使用してスクリプトを読み込み可能にする（外部から挿入されたscriptは実行さない））
+//   res.setHeader(
+//     "Content-Security-Policy",
+//     `default-src 'self'; script-src 'self' 'nonce-${nonce}'`
+//   );
 
-  res.locals.nonce = nonce;
+//   res.locals.nonce = nonce;
 
-  next();
-});
+//   next();
+// });
 
-app.use("/api", cors, apiRouter);
-
+// app.use("/api", cors, apiRouter);
+app.use("/csrf", csrfRouter);
 // サーバを起動する
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
